378f016128cd65fcb15a70af0d8a4396cc7b96a2
BharkaviS <41267360+BharkaviS@users.noreply.github.com>
2020-04-04 21:05:34 +0530

    Update views.py

--- ticket_booking/views.py
+++ ticket_booking/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .forms import MovieDetailForm
+from .forms import MovieDetailFor
 from .models import MovieDetails
 
 # Create your views here.
